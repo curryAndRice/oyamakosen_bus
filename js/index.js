1bf87dbc4c46b17b774e4e43e8d76477d5e0ace9
curryAndRice <takuancurry1248@gmail.com>
2025-10-28 22:22:30 +0900

    整える

--- js/index.js
+++ js/index.js
@@ -1,16 +1,10 @@
-
-//  K13 高専正門 Kosen Seimon 17:19 19:14
-//  中久喜２丁目 Nakakuki-nichome 7:23 9:03 11:18 13:58 15:58
-//  J23 高専正門 Kosen Seimon 6:00 6:45 8:05 9:15 10:15 11:15 12:15 14:35 15:35 16:40 17:50 19:00 20:05
-//  東光 小山高専入口 ["0558","0633","0653","0733","0813","0833","0908","0933","1008","1033","1143","1213","1238","1308","1408","1438","1503","1533","1603","1633","1703","1733","1803 ","1833","1943","2013","2038","2108"]
-
 //対象のバス停の名前
 const stopNames = {
     0:"【高岳線】小山高専入口",
     1:"【城東中久喜線】高専正門",
     2:"【桑東部絹路線 右回り】高専正門", 
     3:"【桑東部絹路線 左回り】中久喜２丁目",
-} ;
+};
 
 //対象のバス停に止まる時間
 const stopTimes = {
@@ -18,7 +12,7 @@ const stopTimes = {
     1:["0600", "0645", "0805", "0915", "1015", "1115", "1215", "1435", "1535", "1640", "1750", "1900", "2005"],
     2:["1719", "1914"],
     3:["0723", "0903", "1118", "1358", "1558"],
-}
+};
 
 const dateTimeDisp = document.getElementById("DateTimeDisp");
 
@@ -62,8 +56,8 @@ const getFirstThree = (time_stamp) => {
             fastTime.push([key, (stopTimes[key][i])])
         }
     }
-    fastTime.sort((a,b) => a[1] - b[1])
-    return [fastTime[0], fastTime[1], fastTime[2]]
+    fastTime.sort((a,b) => a[1] - b[1]);
+    return [fastTime[0], fastTime[1], fastTime[2]];
 }
 
 const update = () =>{
@@ -74,23 +68,22 @@ const update = () =>{
 
     // const [h, m, s] = [6,30,0]
 
-    const firstThree = getFirstThree(getTimeStamp(h, m))
+    const firstThree = getFirstThree(getTimeStamp(h, m));
 
-    let soonText = ""
+    let soonText = "";
 
     for (let i=0; i<3; i++){
         if (firstThree[i] === undefined){
             break;
         }
-        soonText+=firstThree[i][1]
-        soonText+=" に "
-        soonText+=stopNames[firstThree[i][0]]
-        soonText+=" 発<br>"
+        soonText+=firstThree[i][1];
+        soonText+=" に ";
+        soonText+=stopNames[firstThree[i][0]];
+        soonText+=" 発<br>";
     }
 
-    dateTimeDisp.innerHTML = h+"時"+m+"分"+s+"秒";
-    timeStamp.innerHTML = getTimeStamp(h, m);
-    soonBus.innerHTML = soonText
+    dateTimeDisp.innerHTML = h+"時"+m+"分";
+    soonBus.innerHTML = soonText;
 }
 
-update()
+update();
